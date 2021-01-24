04caec0454be472d392176a02396338ccb639f0f
zheng <wuyuxiaotiana@gmail.com>
2021-01-24 13:45:09 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    private static final String _HelloWorld = "HelloWorld";
+    public static void main(String[] args) {
+        System.out.println(_HelloWorld);
+    }
+}
